78665250462e499c50f8d8ab092ae9dd5a40efe5
Nick-Maltais <nickmaltais05@gmail.com>
2026-01-28 17:56:14 -0500

    Finished Lab 2
    I think this should work. Here is my lab 2 R file

---
+++ lab2-git/Lab2_NAM.R
@@ -0,0 +1,9 @@
+setwd("/Users/nicholasmaltais/Bioinformatics/introduction-to-github-classroom-NAMalt/lab2-git")
+
+# ----- Loading and Exploring the iris dataset -----
+
+# Load the dataset -- note that this is a built-in dataset in R
+data(iris)
+
+# View the first few rows to understand its structure
+head(iris)
